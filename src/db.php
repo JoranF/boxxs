85f751daa2db98e7e3221f8811c7806d875c8f42
Joran Floor <joranfloor@gmail.com>
2022-06-01 12:21:37 +0200

    db

--- src/db.php
+++ src/db.php
@@ -1,10 +1,7 @@
 <?php
 class db
 {
-    private $host = 'localhost';
-    private $dbname = 'bloxxs';
-    private $username = 'root';
-    private $password = '';
+
 
     public function __construct()
     {
@@ -13,14 +10,23 @@ class db
 
     private function connect()
     {
+        // try {
+        //     $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
+        //     $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // } catch (PDOException $e) {
+        //     echo "Connection failed: " . $e->getMessage();
+        // }
+
+        // sql lite connection
         try {
-            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
+            $this->conn = new PDO("sqlite:../db.sqlite");
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
         }
-
     }
+
+
 
 
 }
